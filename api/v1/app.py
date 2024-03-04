70055fd05d200d74abd35714fb55545d1977e17a
Navygeta <tinashenavonika@gmail.com>
2024-03-04 03:00:15 +0200

    app.py update

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,7 +15,10 @@ app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
+
+# Updated CORS configuration
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+"""CORS(app, resources={'/*': {'origins': app_host}})"""
 
 
 @app.teardown_appcontext
